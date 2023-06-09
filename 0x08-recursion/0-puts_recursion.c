430fc76fea40d9d8aad0bb1b1f617b592b409c8f
deenyshomar <deenyshomar@gmail.com>
2023-06-08 17:16:07 -0700

    finally fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,17 @@
-#uts_recursion - uses recursion
-* @s: string to recurse through
+#include "main.h"
+
+/**
+ * _puts_recursion - uses recursion
+ * @s: string to recurse through
  */
 
 void _puts_recursion(char *s)
 {
-  if (*s)
-    {
-      _putchar(*s);
-      _puts_recursion(++s);
-    }
-  else
-    _putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar('\n');
 }

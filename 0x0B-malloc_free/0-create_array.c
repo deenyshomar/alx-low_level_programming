cad608f00c0fcfee0e878a61fe3a2757e199dfb2
deenyshomar <deenyshomar@gmail.com>
2023-06-14 06:38:24 -0700

    task 0 betty correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,16 +11,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-  char *t;
-  unsigned int i;
+	char *t;
+	unsigned int i;
 
-  if (size <= 0)
-    return (NULL);
-  t = malloc(sizeof(char) * size);
-  if (t == NULL)
-    return (NULL);
-  for (i = 0; i < size; i++)
-    t[i] = c;
+	if (size <= 0)
+		return (NULL);
+	t = malloc(sizeof(char) * size);
+	if (t == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		t[i] = c;
 
-  return (t);
+	return (t);
 }

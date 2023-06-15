c857d5fca97e4329543932335ac02aff06805590
deenyshomar <deenyshomar@gmail.com>
2023-06-14 17:10:07 -0700

    fixing

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,29 @@
-#include "main.h"
 #include <stdio.h>
-#define UNUSED(x) (void)(x)
+#include <stdlib.h>
 
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - Prints the multiplication of two numbers, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the program receives two arguments - 0.
+ *         If the program does not receive two arguments - 1.
  */
-
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-	int i;
+	int num1, num2, prod;
 
-	for (i = 0; i < argc; i++)
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
+		printf("Error\n");
+		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
 	return (0);
 }
